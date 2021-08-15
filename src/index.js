f8c0dbd33181461f44049a4f4cdebe404068bc7f
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-15 00:24:44 -0700

    Add fn to dynamically create currency select box

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeService from './exchange-service.js';
+import { currencyLibrary } from './currency-library.js';
+
+function createCurrencySelectBox() {
+  let idToAppend = $("#currency");
+  let htmlForSelectBox = `<option value="" selected disabled>Please select a currency</option>`;
+  currencyLibrary.currencies.forEach((currency) => {
+    let currencyCode = currency.currencyCode;
+    let currencyDescription = `${currency.currencyName}, ${currency.country}`;
+    htmlForSelectBox += `<option value="${currencyCode}">${currencyDescription}</option>`;
+  });
+  idToAppend.append(htmlForSelectBox);
+}
